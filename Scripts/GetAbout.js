b09f0aa8d3c7932e942339fce16d74991223bd51
Prof Vickers <dennis.vickers@outlook.com>
2024-04-23 20:49:53 -0700

    Update About

--- Scripts/GetAbout.js
+++ Scripts/GetAbout.js
@@ -9,12 +9,13 @@ import { ConfigSingleton } from "./ConfigFig.js";
     }
   })();
   
-  function updateHTML(configData) {
-    document.querySelector("#headshot").src = "images/".concat(configData.About.Headshot);
-    document.querySelector("#about-name").innerHTML = configData.About.Name;
-    document.querySelector("#about-title").innerHTML = configData.About.Currently;
-    document.querySelector("#pfEmail").innerHTML = configData.Contact.EMail;
-    document.querySelector("#pfPhone").innerHTML = configData.Contact.Phone;
-    document.querySelector("#pfWeb").innerHTML = configData.Contact.Website;
-    
+function updateHTML(configData) {
+  document.querySelector("#pfThumbnail").src = "images/".concat(configData.About.Thumbnail);
+  document.querySelector("#pfHeadshot").src = "images/".concat(configData.About.Headshot);
+  document.querySelector("#pfAboutName").innerHTML = configData.About.Name;
+  document.querySelector("#pfAboutTitle").innerHTML = configData.About.Currently;
+  document.querySelector("#pfAboutDesc").innerHTML = configData.About.AboutText;
+  document.querySelector("#pfEmail").innerHTML = configData.Contact.EMail;
+  document.querySelector("#pfPhone").innerHTML = configData.Contact.Phone;
+  document.querySelector("#pfWeb").innerHTML = configData.Contact.Website;    
 }
