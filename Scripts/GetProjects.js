4c1fa4775e1b3a8fc0a6d9e69ab7a5081b8a55ac
Prof Vickers <dennis.vickers@outlook.com>
2024-04-26 07:29:22 -0700

    Display fixes

--- Scripts/GetProjects.js
+++ Scripts/GetProjects.js
@@ -15,17 +15,17 @@ function updateHTML(configData) {
   document.querySelector("#pfGitHub").href = configData.Contact.GitHub;
   document.querySelector("#pfStack").href = configData.Contact.StackO;
 
-  document.querySelector("#pfProject1Img").src = "content/".concat(configData.Project1.MainImage);
+  document.querySelector("#pfProject1Img").src = "Content/".concat(configData.Project1.MainImage);
   document.querySelector("#pfProject1Title").innerHTML = configData.Project1.Title;
   document.querySelector("#pfProject1Desc").innerHTML = configData.Project1.Desc;
   document.querySelector("#pfProject1Repo").href = configData.Project1.GitHubRepo;
 
-  document.querySelector("#pfProject2Img").src = "content/".concat(configData.Project2.MainImage);
+  document.querySelector("#pfProject2Img").src = "Content/".concat(configData.Project2.MainImage);
   document.querySelector("#pfProject2Title").innerHTML = configData.Project2.Title;
   document.querySelector("#pfProject2Desc").innerHTML = configData.Project2.Desc;
   document.querySelector("#pfProject2Repo").href = configData.Project2.GitHubRepo;
 
-  document.querySelector("#pfProject3Img").src = "content/".concat(configData.Project3.MainImage);
+  document.querySelector("#pfProject3Img").src = "Content/".concat(configData.Project3.MainImage);
   document.querySelector("#pfProject3Title").innerHTML = configData.Project3.Title;
   document.querySelector("#pfProject3Desc").innerHTML = configData.Project3.Desc;
   document.querySelector("#pfProject3Repo").href = configData.Project3.GitHubRepo;
