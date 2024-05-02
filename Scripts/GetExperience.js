790f4bd19e63834b0f427140cd7cfabcc30803a2
Prof Vickers <dennis.vickers@outlook.com>
2024-05-02 13:34:02 -0700

    Release 0.2

--- Scripts/GetExperience.js
+++ Scripts/GetExperience.js
@@ -14,4 +14,29 @@ function updateHTML(configData) {
   document.querySelector("#pfLinkedIn").href = configData.Contact.LinkedIn;
   document.querySelector("#pfGitHub").href = configData.Contact.GitHub;
 
+  // Page Detail
+  document.querySelector("#pfExpText").innerHTML = configData.Experience.ExperienceText;
+
+  document.querySelector("#expEdu>h2").innerHTML = "Education";
+  document.querySelector("#pfSchool").innerHTML = configData.Experience.School;
+  document.querySelector("#pfMajor").innerHTML = configData.Experience.Major;
+  document.querySelector("#pfGraduation").innerHTML = configData.Experience.Graduation;
+  document.querySelector("#pfEducationText").innerHTML = configData.Experience.EducationText;
+
+  document.querySelector("#expEmp>h2").innerHTML = "Employment";
+  document.querySelector("#pfEmployment").innerHTML = configData.Experience.Employment;
+  document.querySelector("#pfRole").innerHTML = configData.Experience.Role;
+  document.querySelector("#pfEmploymentText").innerHTML = configData.Experience.EmploymentText;
+
+  document.querySelector("#expSkills>h2").innerHTML = "Skills";
+  document.querySelector("#pfSkills").innerHTML = configData.Experience.Skills.toString();
+  document.querySelector("#pfSkillsText").innerHTML = configData.Experience.SkillsText;
+
+  // document.querySelector("#pfResume").href = "./Content/".concat(configData.Experience.Resume);
+  // document.querySelector("#pfResume").href = "./Content/resume.pdf";
+  document.querySelector("#pfResume").onclick = () => {
+    let url = "./Content/".concat(configData.Experience.Resume);
+    window.open(url, "_blank");
+  }
+
 }
